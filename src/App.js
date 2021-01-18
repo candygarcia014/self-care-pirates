6c789e9bcb1982d1453da7c04cf0f470d2b6e77b
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-01-17 20:06:43 -0800

    added pages, made navbar look better and responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
-import NavBar from "./components/Navbar" 
+import React from 'react';
+import './App.css';
+import NavBar from '../src/components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
   return (
-
-    <NavBar />
-  
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact-us' component={Contact} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
