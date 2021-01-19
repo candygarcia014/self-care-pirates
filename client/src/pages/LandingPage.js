17a2822a60c02f277cabb1d6644ccfba5ccb9c42
Snax <shanni.c.suissa@gmail.com>
2021-01-19 10:38:23 -0800

    fixing page names

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const About = () => {
+const LandingPage = () => {
   return (
     <div
       style={{
@@ -10,9 +10,9 @@ const About = () => {
         height: '90vh'
       }}
     >
-      <h1>About new</h1>
+      <h1>Home NEW</h1>
     </div>
   );
 };
 
-export default About;
+export default LandingPage;
