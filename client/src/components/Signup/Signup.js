59c057bfe38bb02ce729a94b5c54c638496f18ab
unknown <candygarcia014@gmail.com>
2021-01-20 13:35:38 -0800

    added functionality to the sig-up page

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -5,6 +5,10 @@ import Api from "../../utils/Api"
 
 const Signup = (props) => {
   const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [userName, setUserName] = useState("")
   return (
     <Form>
       <Row form>
@@ -17,30 +21,30 @@ const Signup = (props) => {
         <Col md={6}>
           <FormGroup>
             <Label for="LastName">Last Name</Label>
-            <Input type="LastName" name="LastName" id="LastName" placeholder="Last Name" />
+            <Input type="LastName" name="LastName" id="LastName" placeholder="Last Name" onChange = { e => setLastName (e.target.value)} value = {lastName} />
           </FormGroup>
         </Col>
       </Row>
       <FormGroup>
-        <Label for="Email">Email</Label>
-        <Input type="email" name="Email" id="Email" placeholder="Email"/>
+        <Label for="email">Email</Label>
+        <Input type="email" name="email" id="mail" placeholder="Email" onChange = { e => setEmail (e.target.value)} value = {email}/>
       </FormGroup>
       <FormGroup>
         <Label for="password">Password </Label>
-        <Input type="password" name="password" id="password" placeholder="Password"/>
+        <Input type="password" name="password" id="password" placeholder="Password" onChange = { e => setPassword (e.target.value)} value = {password}/>
       </FormGroup>
       <Row form>
         <Col>
           <FormGroup>
-            <Label for="UserName">User Name</Label>
-            <Input type="text" name="UserName" id="User Name"/>
+            <Label for="userName">Username</Label>
+            <Input type="text" name="userName" id="User Name" placeholder="Create a cool username" onChange = { e => setUserName (e.target.value)} value = {userName}/>
           </FormGroup>
         </Col>
       </Row>
-      <FormGroup check>
+      {/* <FormGroup check>
         <Input type="checkbox" name="check" id="exampleCheck"/>
         <Label for="exampleCheck" check>Check me out</Label>
-      </FormGroup>
+      </FormGroup> */}
       <Button>Sign in</Button>
     </Form>
   );
