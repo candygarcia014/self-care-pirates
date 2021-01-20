d771af0013465d9053920464dca61dca73e02149
unknown <candygarcia014@gmail.com>
2021-01-20 13:12:26 -0800

    first name state

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './Signup.css';
+import Api from "../../utils/Api"
 
 const Signup = (props) => {
+  const [firstName, setFirstName] = useState("")
   return (
     <Form>
       <Row form>
         <Col md={6}>
           <FormGroup>
             <Label for="FirstName">First Name</Label>
-            <Input type="FirstName" name="FirstName" id="FirstName" placeholder="First Name" />
+            <Input type="FirstName" name="FirstName" id="FirstName" placeholder="First Name" onChange = { e => setFirstName (e.target.value)} value = {firstName}/>
           </FormGroup>
         </Col>
         <Col md={6}>
