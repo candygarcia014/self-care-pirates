4a567f7ff57ba147bed7382204c3d3f3e26bf39c
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-01-21 02:05:33 -0800

    started coding our story but it late to finish

--- client/src/pages/OurStory/OurStory.js
+++ client/src/pages/OurStory/OurStory.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
-const OurStory = () => {
+
+const OurStory = (props) => {
   return (
-    <Container fluid>
-  <Row>
-    <Col>1 of 1</Col>
-  </Row>
-</Container>
+    <div>
+      <Card>
+        <CardImg src="https://raw.githubusercontent.com/ShanniSnax/self-care-pirates/main/client/src/components/images/AvatarExample.jpgZ" alt="Card image cap" />
+        <CardBody>
+          <CardTitle tag="h5">Card title</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
   );
 };
 
