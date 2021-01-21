07acbb9eeff452a4cbb4a16964bdeb05467d2f55
ttngu <tammyn137@gmail.com>
2021-01-21 14:26:49 -0800

    removed old code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -54,30 +54,7 @@ const NavBar = () => {
         
       </Navbar.Collapse>
     </Navbar>
- 
-      // <Nav>
-      //   <NavLink to='/' activeStyle>
-      //     <img src ={LogoCropped.default} alt="logo" style={{"height":"3rem", "margin":"0.5rem"}}/>
-      //   </NavLink>
-      //   <Bars />
-      //   <NavMenu>
-      //     <NavLink to='/forum' activeStyle>
-      //       Forum
-      //     </NavLink>
-      //     <NavLink to='/our-story' activeStyle>
-      //       Our Story
-      //     </NavLink>
-      //     <NavLink to='/user' activeStyle>
-      //      User
-      //     </NavLink>
-      //   </NavMenu>
-        
-      //   <NavBtn className="ml-auto">
-      //     {/* Will display "Profile" once logged in */}
-      //     <NavBtnLink to='/Register' style={{"border":"black solid 1px"}} >Login / Sign Up</NavBtnLink>
-      //   </NavBtn>
-      // </Nav>
-  );
+   );
 };
 
 export default NavBar;
