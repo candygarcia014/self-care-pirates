857d66e112f444d650b8d61bb985b104b183b94e
CGC <70499508+cgconverse@users.noreply.github.com>
2021-01-22 01:20:51 -0800

    deleted our story text from storycard

--- client/src/components/StoryCard/StoryCard.js
+++ client/src/components/StoryCard/StoryCard.js
@@ -7,7 +7,6 @@ const StoryCard = () => {
   
   <Card className="story-card">
   <Card.Body>
-    <Card.Title>Our Story</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
