e00b966e240f2e5f36cbaf3fba085070f9fc329d
CGC <70499508+cgconverse@users.noreply.github.com>
2021-01-27 01:14:53 -0800

    added css to dev cards

--- client/src/components/DevCard/DevCard.js
+++ client/src/components/DevCard/DevCard.js
@@ -1,61 +1,37 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-// import { Container } from 'react-bootstrap';
 import "./DevCard.css"
+import '../../developers.json'
 
-// const styles = {
-//     card: {
-//       margin: 20,
-//       background: "#e8eaf6"
-//     },
-//     heading: {
-//       background: "#3f51b5",
-//       minHeight: 50,
-//       lineHeight: 3.5,
-//       fontSize: "1.2rem",
-//       color: "white",
-//       padding: "0 20px"
-//     },
-//     content: {
-//       padding: 20
-//     }
-//   };
 
-// const styles = {
-//     image: {
-//         width: '243px', 
-//         height: '240px', 
-//         top: '886px',
-//         left: '248px',
-//         radius: '256px'
-//     }
-// };
-
-// const Image = () => {
-//     return (
-//         <Container>
-//         <img style={styles.image} src="https://picsum.photos/200"/>
-//         </Container>
-//     )
-// };
 
 const DevCard = (props) => {
     return (
 
-        <Card style={{maxHeight: "20rem !important", maxWidth: "50rem" }}>
+        <Card className="dev-card" style={{maxHeight: "20rem !important", maxWidth: "50rem" }}>
             {/* <Card.Img style={styles.image} variant="top" src="https://picsum.photos/200" /> */}
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
-                <a href={props.github}>
+                {/* <a href={props.github}>
                     <Card.Text>
                         {props.github}
+                    </Card.Text>
+                </a> */}
+                <a href={props.github}>
+                    <Card.Text>
+                       <a href={props.github}><img src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/96/iconmonstr-github-3.png"/></a> 
                     </Card.Text>
                 </a>
                 <a href={props.linkedin}>
                     <Card.Text>
-                        {props.linkedin}
+                       <a href={props.linkedin}><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"/></a> 
                     </Card.Text>
                 </a>
+                {/* <a href={props.linkedin}>
+                    <Card.Text>
+                       <a href={props.linkedin}><img src="https://cdn5.vectorstock.com/i/thumb-large/99/09/linkedin-social-media-icon-design-template-vector-22339909.jpg"/></a> 
+                    </Card.Text>
+                </a> */}
             </Card.Body>
         </Card>
     )
