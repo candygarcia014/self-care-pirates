cbad7b3c52c0c1fd4d56ac3adc34a650e6ec41d3
CGC <70499508+cgconverse@users.noreply.github.com>
2021-01-27 02:28:09 -0800

    fixed sizing of logos

--- client/src/components/DevCard/DevCard.js
+++ client/src/components/DevCard/DevCard.js
@@ -9,29 +9,21 @@ const DevCard = (props) => {
     return (
 
         <Card className="dev-card" style={{maxHeight: "20rem !important", maxWidth: "50rem" }}>
-            {/* <Card.Img style={styles.image} variant="top" src="https://picsum.photos/200" /> */}
+        
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
-                {/* <a href={props.github}>
-                    <Card.Text>
-                        {props.github}
-                    </Card.Text>
-                </a> */}
+            
                 <a href={props.github}>
                     <Card.Text>
-                       <a href={props.github}><img className="github" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/96/iconmonstr-github-3.png"/></a> 
+                       <a href={props.github}><img className="github" src="https://github.com/ShanniSnax/self-care-pirates/blob/main/client/src/components/images/github-icon.png?raw=true"/></a> 
                     </Card.Text>
                 </a>
                 <a href={props.linkedin}>
                     <Card.Text>
-                       <a href={props.linkedin}><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"/></a> 
+                       <a href={props.linkedin}><img src="https://github.com/ShanniSnax/self-care-pirates/blob/main/client/src/components/images/linkedin-icon.png?raw=true"/></a> 
                     </Card.Text>
                 </a>
-                {/* <a href={props.linkedin}>
-                    <Card.Text>
-                       <a href={props.linkedin}><img src="https://cdn5.vectorstock.com/i/thumb-large/99/09/linkedin-social-media-icon-design-template-vector-22339909.jpg"/></a> 
-                    </Card.Text>
-                </a> */}
+            
             </Card.Body>
         </Card>
     )
